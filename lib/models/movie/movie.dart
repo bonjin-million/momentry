3ac3133e3ec318c85115c2da953cdcbf689f5a5f
jinhee <wlsgml8989@gmail.com>
2023-07-09 23:55:36 +0900

    refactor: movie.dart 영화검색 API 변경

--- lib/models/movie/movie.dart
+++ lib/models/movie/movie.dart
@@ -1,106 +1,154 @@
+// class Movie {
+//   final String title; // 영화명
+//   final String prodYear; // 제작년도
+//   final String detailUrl; // 링크URL
+//   final List<MoviePoster> posters;
+//   final List<MovieStill> stills;
+//   final List<MovieActor> actors;
+//   final List<MovieDirector> directors;
+//
+//   Movie({
+//     required this.title,
+//     required this.prodYear,
+//     required this.detailUrl,
+//     required this.posters,
+//     required this.stills,
+//     required this.actors,
+//     required this.directors,
+//   });
+//
+//   Movie.fromJson(Map<String, dynamic> json)
+//       : title = json['title'].replaceAll("!HS ", "").replaceAll("!HE ", ""),
+//         prodYear = json['prodYear'],
+//         detailUrl = json['kmdbUrl'],
+//         posters = json['posters']
+//             .toString()
+//             .split("|")
+//             .map((e) => MoviePoster(imageUrl: e))
+//             .toList(),
+//         stills = json['stills']
+//             .toString()
+//             .split("|")
+//             .map((e) => MovieStill(imageUrl: e))
+//             .toList(),
+//         actors = (json['actors']["actor"] as List)
+//             .map((e) => MovieActor.fromJson(e))
+//             .toList(),
+//         directors = (json['directors']["director"] as List)
+//             .map((e) => MovieDirector.fromJson(e))
+//             .toList();
+// }
+//
+// class MoviePoster {
+//   final String imageUrl;
+//
+//   MoviePoster({required this.imageUrl});
+//
+//   MoviePoster.fromJson(Map<String, dynamic> json) : imageUrl = json['imageUrl'];
+// }
+//
+// class MovieStill {
+//   final String imageUrl;
+//
+//   MovieStill({required this.imageUrl});
+//
+//   MovieStill.fromJson(Map<String, dynamic> json) : imageUrl = json['imageUrl'];
+// }
+//
+// class MovieDirector {
+//   final String directorNm;
+//   final String directorEnNm;
+//   final String directorId;
+//
+//   MovieDirector({
+//     required this.directorNm,
+//     required this.directorEnNm,
+//     required this.directorId,
+//   });
+//
+//   factory MovieDirector.fromJson(Map<String, dynamic> json) => MovieDirector(
+//       directorNm: json['directorNm'],
+//       directorEnNm: json['directorEnNm'],
+//       directorId: json['directorId']);
+//
+//   Map<String, dynamic> toJson() {
+//     return {
+//       'directorNm': directorNm,
+//       'directorEnNm': directorEnNm,
+//       'directorId': directorId,
+//     };
+//   }
+// }
+//
+// class MovieActor {
+//   final String actorNm;
+//   final String actorEnNm;
+//   final String actorId;
+//
+//   MovieActor({
+//     required this.actorNm,
+//     required this.actorEnNm,
+//     required this.actorId,
+//   });
+//
+//   factory MovieActor.fromJson(Map<String, dynamic> json) => MovieActor(
+//       actorNm: json['actorNm'],
+//       actorEnNm: json['actorEnNm'],
+//       actorId: json['actorId']);
+//
+//   Map<String, dynamic> toJson() {
+//     return {
+//       'actorNm': actorNm,
+//       'actorEnNm': actorEnNm,
+//       'actorId': actorId,
+//     };
+//   }
+// }
 class Movie {
-  final String title; // 영화명
-  final String prodYear; // 제작년도
-  final String detailUrl; // 링크URL
-  final List<MoviePoster> posters;
-  final List<MovieStill> stills;
-  final List<MovieActor> actors;
-  final List<MovieDirector> directors;
+  final bool adult;
+  final String? backdropPath;
+  final List<dynamic> genreIds;
+  final int id;
+  final String originalLanguage;
+  final String originalTitle;
+  final String overview;
+  final double popularity;
+  final String posterPath; //이미지 경로
+  final String releaseDate;
+  final String title;
+  final bool video;
+  final double voteAverage;
+  final int voteCount;
 
-  Movie({
-    required this.title,
-    required this.prodYear,
-    required this.detailUrl,
-    required this.posters,
-    required this.stills,
-    required this.actors,
-    required this.directors,
-  });
+  Movie(
+      this.adult,
+      this.backdropPath,
+      this.genreIds,
+      this.id,
+      this.originalLanguage,
+      this.originalTitle,
+      this.overview,
+      this.popularity,
+      this.posterPath,
+      this.releaseDate,
+      this.title,
+      this.video,
+      this.voteAverage,
+      this.voteCount);
 
   Movie.fromJson(Map<String, dynamic> json)
-      : title = json['title'].replaceAll("!HS ", "").replaceAll("!HE ", ""),
-        prodYear = json['prodYear'],
-        detailUrl = json['kmdbUrl'],
-        posters = json['posters']
-            .toString()
-            .split("|")
-            .map((e) => MoviePoster(imageUrl: e))
-            .toList(),
-        stills = json['stills']
-            .toString()
-            .split("|")
-            .map((e) => MovieStill(imageUrl: e))
-            .toList(),
-        actors = (json['actors']["actor"] as List)
-            .map((e) => MovieActor.fromJson(e))
-            .toList(),
-        directors = (json['directors']["director"] as List)
-            .map((e) => MovieDirector.fromJson(e))
-            .toList();
-}
-
-class MoviePoster {
-  final String imageUrl;
-
-  MoviePoster({required this.imageUrl});
-
-  MoviePoster.fromJson(Map<String, dynamic> json) : imageUrl = json['imageUrl'];
-}
-
-class MovieStill {
-  final String imageUrl;
-
-  MovieStill({required this.imageUrl});
-
-  MovieStill.fromJson(Map<String, dynamic> json) : imageUrl = json['imageUrl'];
-}
-
-class MovieDirector {
-  final String directorNm;
-  final String directorEnNm;
-  final String directorId;
-
-  MovieDirector({
-    required this.directorNm,
-    required this.directorEnNm,
-    required this.directorId,
-  });
-
-  factory MovieDirector.fromJson(Map<String, dynamic> json) => MovieDirector(
-      directorNm: json['directorNm'],
-      directorEnNm: json['directorEnNm'],
-      directorId: json['directorId']);
-
-  Map<String, dynamic> toJson() {
-    return {
-      'directorNm': directorNm,
-      'directorEnNm': directorEnNm,
-      'directorId': directorId,
-    };
-  }
-}
-
-class MovieActor {
-  final String actorNm;
-  final String actorEnNm;
-  final String actorId;
-
-  MovieActor({
-    required this.actorNm,
-    required this.actorEnNm,
-    required this.actorId,
-  });
-
-  factory MovieActor.fromJson(Map<String, dynamic> json) => MovieActor(
-      actorNm: json['actorNm'],
-      actorEnNm: json['actorEnNm'],
-      actorId: json['actorId']);
-
-  Map<String, dynamic> toJson() {
-    return {
-      'actorNm': actorNm,
-      'actorEnNm': actorEnNm,
-      'actorId': actorId,
-    };
-  }
+      : title = json['title'],
+        adult = json['adult'],
+        backdropPath = json['backdrop_path'],
+        genreIds = json['genre_ids'],
+        id = json['id'],
+        originalLanguage = json['original_language'],
+        originalTitle = json['original_title'],
+        overview = json['overview'],
+        popularity = json['popularity'],
+        posterPath = 'https://image.tmdb.org/t/p/w200' + json['poster_path'],
+        releaseDate = json['release_date'],
+        video = json['video'],
+        voteAverage = json['vote_average'],
+        voteCount = json['vote_count'];
 }
--- lib/repositories/movie_repository.dart
+++ lib/repositories/movie_repository.dart
@@ -8,11 +8,21 @@ import 'dart:core';
 import 'package:momentry/models/movie/movie_response.dart';
 
 class MovieRepository {
-  final apiKey = "Z8FMIU652ZR9ZAEVQDO3";
+  // final apiKey = "Z8FMIU652ZR9ZAEVQDO3";
 
+  final apiKey = "748a988dd7391c38f054a669ae678d1b";
   Future<MovieResponse> fetchItems({required String keyword}) async {
-    final uri = Uri.parse('http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&listCount=50&ServiceKey=$apiKey&title=$keyword');
+    // final uri = Uri.parse(
+    //     'http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&detail=Y&listCount=50&ServiceKey=$apiKey&title=$keyword');
+    final uri = Uri.parse(
+        'https://api.themoviedb.org/3/search/movie?api_key=$apiKey&query=$keyword&language=ko-KR');
+
+    // 'https://api.themoviedb.org/3/search/movie?api_key=748a988dd7391c38f054a669ae678d1b&page=1&query=%EB%B2%94%EC%A3%84&language=ko-KR');
+
     final response = await http.get(uri);
+
+    print("@@@@@@@ ${MovieResponse.fromJson(jsonDecode(response.body))}");
+
     return MovieResponse.fromJson(jsonDecode(response.body));
   }
 
@@ -22,11 +32,24 @@ class MovieRepository {
   }
 
   Future<MovieDetail> fetchDetailItem(int id) async {
-    final response = await MovieDatabase().findById(id);
-    return response;
+    // final response = await MovieDatabase().findById(id);
+    // return response;
+
+    final uri = Uri.parse(
+        'https://api.themoviedb.org/3/search/movie?api_key=$apiKey&query=$id&language=ko-KR');
+
+    final response = await http.get(uri);
+
+    print("@@@@@@@ ${MovieDetail.fromJson(jsonDecode(response.body))}");
+
+    return MovieDetail.fromJson(jsonDecode(response.body));
   }
 
   Future<void> add(Map<String, dynamic> data) async {
     await MovieDatabase().insert(data);
+  }
+
+  Future<void> delete(int id) async {
+    await MovieDatabase().delete(id);
   }
 }
